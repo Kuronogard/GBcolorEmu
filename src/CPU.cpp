ecad339c10cd9eb2ef19266e23f0370dd6ccfdc4
Kuronogard <dpinto@ucm.es>
2017-10-29 11:25:44 +0100

    Implemented CPU.run()

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -21,14 +21,54 @@ GbaEmuCpu::~GbaEmuCpu()
 
 bool GbaEmuCpu::run()
 {
+    memValue8_t opcode;
+    Instruction currentInstruction;
+    uint8_t numParamBytes;
+    memValue16_t param;
+    bool executeSuccess;
 
     // each cycle
+    while(true)
+    {
+        // wait until next cycle
+        // Configure cycle period
+
         // read the byte pointed by regPC
+        opcode = memoryController.read(registers.PC);
+
         // compare it with the opcodes of the instructions until a match is found
+        currentInstruction = decode(opcode);
+
         // wait the number of delay cycles
-        // read the instruction params
-        // update PC
+        delayCycles = currentInstruction.numDelayCycles();
+        // wait(delayCycles)
+
+        // read the instruction params and update PC
+        numParamBytes = currentInstruction.numParamBytes();
+        switch (numParamBytes)
+        {
+        case 2:
+            param = memoryController.read(registers.PC+1);
+            param |= (memoryController.read(registers.PC+2) << 8);
+            registers.PC += 3;
+            break;
+        case 1:
+            param = memoryController.read(registers.PC+1);
+            registers.PC += 2;
+            break;
+        case 0:
+            param = 0;
+            registers.PC += 1;
+            break;
+        }
+
         // execute the instruction
+        executeSuccess = currentInstruction.execute(registers, memoryController, param);
+        if (!executeSuccess)
+        {
+            return false;
+        }
+    }
 
     return true;
 }
