eb3835530f95c8920634a3075b118ef67690d737
Kuronogard <dpinto@ucm.es>
2017-10-29 10:22:12 +0100

    Documentation in CPU

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -7,7 +7,7 @@ namespace gbcoloremu
 
 GbaEmuCpu::GbaEmuCpu()
 {
-
+    // Create instruction array and load all the processor instruction
 
 }
 
@@ -21,6 +21,14 @@ GbaEmuCpu::~GbaEmuCpu()
 
 bool GbaEmuCpu::run()
 {
+
+    // each cycle
+        // read the byte pointed by regPC
+        // compare it with the opcodes of the instructions until a match is found
+        // wait the number of delay cycles
+        // read the instruction params
+        // update PC
+        // execute the instruction
 
     return true;
 }
--- src/CPU.hpp
+++ src/CPU.hpp
@@ -7,9 +7,28 @@
 namespace gbcoloremu
 {
 
-class GbaEmuCpu {
+using regValue_t = uint8_t;
+using doubleRegValue_t = uint16_t;
+
+class GbaEmuCpu
+{
 private:
 
+    // Posible 16bit pairings  (15..8 | 7..0)
+    //                              A - F
+    //                              B - C
+    //                              D - E
+    //                              H - L
+
+    // regF stores results of various math operations
+    // PC points to the next instruction to be executed (initial value: 0x0100)
+    // SP points to the current stack position (initial value: 0xFFFE)
+    regValue_t regA, regB, regC, regD, regE, regF;
+    regValue_t regH, regL;
+    doubleRegValue_t regSP, regPC;
+
+    // SP grows downward in RAM memory.
+    // It decrements automatically before something is put in it
     MemoryController memoryController;
 
 public:
