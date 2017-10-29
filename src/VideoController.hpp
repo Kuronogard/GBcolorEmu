bd56b613ff895b4a49f1daa9b5c4c705650a4993
Kuronogard <dpinto@ucm.es>
2017-10-29 10:23:20 +0100

    Documentation in video controller

--- src/VideoController.hpp
+++ src/VideoController.hpp
@@ -2,6 +2,19 @@
 #ifndef __VIDEO_CONTROLLER_HPP
 #define __VIDEO_CONTROLLER_HPP
 
+
+// Screen buffer (background) 256x256 pixels -> 32x32 tiles (8x8 pixel/tiles)
+// only 160x144 pixels displayed on screen -> SCROLLX and SCROLLY are the left upper corner
+// background wraps around the screen
+
+// Background Tile Map:  32 rows of 32 bytes (each contains the number of a til to be displayed)
+
+// Tile Data Table :($8000 - $8FFF) or ($8800 - 97FF)
+        // $8000 - $8FFF: patterns numbered from 0 to 255
+        // $8800 - $97FF: patterns numbered from -128 to 127
+
+// Window overlayng the background (WINDPOSX and WINDPOSY are the left upper corner)
+
 namespace gbcoloremu
 {
 class VideoController {
