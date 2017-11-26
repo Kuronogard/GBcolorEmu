81c42ad2dfa8d67b290b109d838b9b4de6f2e7d6
Kuronogard <dpinto@ucm.es>
2017-11-26 18:17:44 +0100

    Added empty implementations of all the flowinstructions

--- src/flowInstructions.cpp
+++ src/flowInstructions.cpp
@@ -5,4 +5,186 @@ namespace gbcoloremu
 
 
 
+bool Instr_JR_NZ_n::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JR_NC_n::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JR_n::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JR_Z_n::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JR_C_n::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RET_NZ::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RET_NC::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RET_Z::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RET_C::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RET::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RETI::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_NZ_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_NC_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_Z_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_C_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_JP_HL::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_CALL_NZ_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_CALL_NC_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_CALL_Z_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_CALL_C_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_CALL_nn::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_00::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_08::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_10::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_18::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_20::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_28::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_30::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+bool Instr_RST_38::execute(RegisterBank &registers, MemoryController &memory, doubleRegValue_t pc, memValue8_t opcode)
+{
+
+}
+
+
+
+
 } /* namespace gbcoloremu */
